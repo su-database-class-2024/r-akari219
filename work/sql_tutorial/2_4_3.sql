f30ba2e8a970ea380b40dae9bc0ca7d1ffcb3d56
akari <akarispecial@icloud.com>
2024-12-05 12:30:25 +0900

    12/05 22TI057:柿沼燈 チュートリアル課題 2_9_2まで

--- work/sql_tutorial/2_4_3.sql
+++ work/sql_tutorial/2_4_3.sql
@@ -1,2 +1,2 @@
-INSERT INTO weather (date, city, temp_hi, temp_lo)
-    VALUES ('1994-11-29', 'Hayward', 54, 37);
+INSERT INTO weather (city, temp_lo, temp_hi, prcp, date)
+    VALUES ('San Francisco', 43, 57, 0.0, '1994-11-29');
